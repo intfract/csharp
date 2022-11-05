51ddd93d46315309a63c1380bbb846a8d4ed278e
Int Fract <110207136+intfract@users.noreply.github.com>
2022-11-05 08:34:19 +0000

    added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# C# 
+
+An experimental C# project! 
--- main.cs
+++ main.cs
@@ -1,47 +1,63 @@
 using System;
 using System.Collections.Generic;
 
-class Program {
-  static void print(dynamic message) {
-    if (message is List<dynamic>) {
-      for (int i = 0; i < message.Count; i++) {
-        Console.WriteLine($"List [{i}] = {message[i]}");
-      }
-    } else {
-      Console.WriteLine(message);
+class Program
+{
+    static void print(dynamic message)
+    {
+        try
+        {
+            for (int i = 0; i < message.Count; i++)
+            {
+                Console.WriteLine($"List [{i}] = {message[i]}");
+            }
+        }
+        catch
+        {
+            Console.WriteLine(message.ToString());
+        }
     }
-  }
 
-  static int sortedIndex(List<dynamic> items, dynamic item) {
-    int start = 0;
-    int end = items.Count;
-    int mid = (start + end) / 2;
-    int i = 0;
-    double depth = Math.Log(items.Count + 1, 2);
-    while (i <= Math.Ceiling(depth)) {
-      mid = (start + end) / 2;
-      if (start >= end) {
-        break;
-      }
-      if (item == items[mid]) {
+    static int sortedIndex(dynamic items, dynamic item)
+    {
+        int start = 0;
+        int end = items.Count;
+        int mid = (start + end) / 2;
+        int i = 0;
+        double depth = Math.Log(items.Count + 1, 2);
+        while (i <= Math.Ceiling(depth))
+        {
+            mid = (start + end) / 2;
+            if (start >= end)
+            {
+                break;
+            }
+            if (item == items[mid])
+            {
+                return mid;
+            }
+            else if (item > items[mid])
+            {
+                start = mid + 1;
+            }
+            else if (item < items[mid])
+            {
+                end = mid;
+            }
+        }
         return mid;
-      } else if (item > items[mid]) {
-        start = mid + 1;
-      } else if (item < items[mid]) {
-        end = mid;
-      }
     }
-    return mid;
-  }
 
-  static List<dynamic> sortedInsert(List<dynamic> items, dynamic item) {
-    items.Insert(sortedIndex(items, item), item);
-    return items;
-  }
-  
-  public static void Main (string[] args) {
-    List<dynamic> items = new List<dynamic>() { 0, 2, 4 };
-    print(sortedIndex(items, 1));
-    print(sortedInsert(items, 1));
-  }
+    static dynamic sortedInsert(dynamic items, dynamic item)
+    {
+        items.Insert(sortedIndex(items, item), item);
+        return items;
+    }
+
+    public static void Main(string[] args)
+    {
+        var items = new List<int>() { 0, 2, 4 };
+        print(sortedIndex(items, 1));
+        sortedInsert(items, 1);
+    }
 }
